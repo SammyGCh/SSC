ddf38b17ee116873f33ebd694ff3f66b794a5617
SammyGCh <53235299+SammyGCh@users.noreply.github.com>
2020-07-08 22:58:39 -0500

    creacion del proyecto

---
+++ AccesoADatos/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AccesoADatos
+{
+    public class Class1
+    {
+    }
+}
---
+++ DominioNegocio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DominioNegocio
+{
+    public class Class1
+    {
+    }
+}
