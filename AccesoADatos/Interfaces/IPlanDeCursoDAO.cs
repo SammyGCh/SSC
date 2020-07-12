aee243b897c4f4c4fca482e61ae12bb4b6a1e899
Angel <inscripangel@hotmail.com>
2020-07-12 18:50:18 -0500

    Creacion de interfaces
    Se hicieron las interfaces basicas de las clases del dominio

---
+++ AccesoADatos/Interfaces/IPlanDeCursoDAO.cs
@@ -0,0 +1,12 @@
+﻿using DominioNegocio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccesoADatos.Interfaces
+{
+    public interface IPlanDeCursoDAO
+    {
+        List<PlanDeCurso> ObtenerPlanesPorCurso(String nombre);
+    }
+}
---
+++ AccesoADatos/Interfaces/ISolicitudCambioDAO.cs
@@ -0,0 +1,12 @@
+﻿using DominioNegocio;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccesoADatos.Interfaces
+{
+    public interface ISolicitudCambioDAO
+    {
+        bool RegistrarSolicitud(SolicitudCambio nuevaSolicitud);
+    }
+}
