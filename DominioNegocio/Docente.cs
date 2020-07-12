91e035c8ba66d6ce3e00b3dc76fdb0e2eddbf403
SammyGCh <53235299+SammyGCh@users.noreply.github.com>
2020-07-11 21:39:26 -0500

    creacion de clases del dominio

---
+++ DominioNegocio/Docente.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DominioNegocio
+{
+    public class Docente : Usuario
+    {
+        private int idDocente;
+        private string aniosExperiencia;
+        private string curp;
+        private string fechaNacimiento;
+        private string numeroPersonal;
+        private string perfilProfesional;
+        private string rfc;
+
+        public int IdDocente
+        {
+            get => idDocente;
+            set => idDocente = value;
+        }
+
+        public string AniosExperiencia
+        {
+            get => aniosExperiencia;
+            set => aniosExperiencia = value;
+        }
+
+        public string Curp
+        {
+            get => curp;
+            set => curp = value;
+        }
+
+        public string FechaNacimiento
+        {
+            get => fechaNacimiento;
+            set => fechaNacimiento = value;
+        }
+
+        public string NumeroPersonal
+        {
+            get => numeroPersonal;
+            set => numeroPersonal = value;
+        }
+
+        public string PerfilProfesional
+        {
+            get => perfilProfesional;
+            set => perfilProfesional = value;
+        }
+
+        public string Rfc
+        {
+            get => rfc;
+            set => rfc = value;
+        }
+    }
+}
---
+++ DominioNegocio/TipoUsuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DominioNegocio
+{
+    public class TipoUsuario
+    {
+        private int idTipoUsuario;
+        private string nombre;
+
+        public int IdTipoUsuario
+        {
+            get => idTipoUsuario;
+            set => idTipoUsuario = value;
+        }
+
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+    }
+}
